87ef31c9e70e7fd0117d61797879b4f17ed78ed6
Ilan Smith <ilansm@mellanox.com>
2017-07-19 13:53:13 +0000

    Create sa and socket generically

--- udp_client.cpp
+++ udp_client.cpp
@@ -22,14 +22,61 @@ void error(const char *msg) {
 	exit(0);
 }
 
+static int my_getaddrinfo(const char *host, const char *port, int *sockfd,
+	struct sockaddr **sa, socklen_t *salen)
+{
+	struct addrinfo hints, *res, *ressave;
+	int n, ret = -1;
+
+	/*initilize addrinfo structure*/
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+
+	if ((n = getaddrinfo(host, port, &hints, &res)) != 0) {
+		printf("udpclient error for %s, %s: %s", host, port,
+			gai_strerror(n));
+		return -1;
+	}
+
+	ressave = res;
+	if (sockfd) {
+		do {/* each of the returned IP address is tried*/
+			int tmp;
+
+			tmp = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+			if (tmp >= 0) {
+				printf("inet_ntoa(((struct sockaddr_in*)res->ai_addr)->sin_addr): %s\n",
+						inet_ntoa(((struct sockaddr_in*)res->ai_addr)->sin_addr));
+				*sockfd = tmp;
+				break; /*success*/
+			}
+		} while ((res = res->ai_next) != NULL);
+
+		if (!res) {
+			printf("Could not open a socket for %s:%s\n", host, port);
+			goto Exit;
+		}
+	}
+
+	*sa = (struct sockaddr*)malloc(res->ai_addrlen);
+	memcpy(*sa, res->ai_addr, res->ai_addrlen);
+	*salen = res->ai_addrlen;
+
+	ret = 0;
+Exit:
+	freeaddrinfo(ressave);
+	return ret;
+}
+
 int main(int argc, char *argv[])
 {
 	int sockfd, n, rc;
-	socklen_t salen;
-	struct sockaddr *sa;
+	socklen_t salen, salen_local;
+	struct sockaddr *sa, *sa_local;
 	struct sockaddr_in sa_in;
 	socklen_t sin_len;
-	struct addrinfo hints, *res, *ressave;
 	char *host, *port;
 	int i;
 	int fromlen;
@@ -40,40 +87,16 @@ int main(int argc, char *argv[])
 	nonacks = 0;
 	acks = 0;
 
-	if (argc < 3) {
+	if (argc < 3 || argc > 4) {
 		printf("usage: %s <hostname/IPaddress> <portnumber> [rate] \n",
 				argv[0]);
-		exit(0);
+		exit(-1);
 	}
 
 	host = argv[1];
 	port = argv[2];
 
-	/*initilize addrinfo structure*/
-	bzero(&hints, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_DGRAM;
-	hints.ai_protocol = IPPROTO_UDP;
-
-	if ((n = getaddrinfo(host, port, &hints, &res)) != 0)
-		printf("udpclient error for %s, %s: %s", host, port, gai_strerror(n));
-	ressave = res;
-
-	do {/* each of the returned IP address is tried*/
-		sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-		if (sockfd >= 0)
-			break; /*success*/
-	} while ((res = res->ai_next) != NULL);
-
-	sa = (struct sockaddr *) malloc(res->ai_addrlen);
-	memcpy(sa, res->ai_addr, res->ai_addrlen);
-	salen = res->ai_addrlen;
-
-	sin_len = sizeof(sa_in);
-	getsockname(sockfd, (struct sockaddr*) &sa_in, &sin_len);
-	// use in_addr?
-
-	freeaddrinfo(ressave);
+	my_getaddrinfo(host, port, &sockfd, &sa, &salen);
 
 	if (argc > 3) {
 		uint64_t rate = atol(argv[3]);
