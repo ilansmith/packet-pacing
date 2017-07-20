f7b3ed3d7443a29252e5218a6cc3bc41853dfd42
Or Shalev <orsh@mellanox.com>
2017-07-20 10:34:21 +0300

    improved sleep timing, code cleanup
    Signed-off-by: Or Shalev <orsh@mellanox.com>

--- udp_client.cpp
+++ udp_client.cpp
@@ -10,13 +10,29 @@
 #include <iostream>
 #include <errno.h>
 #include <math.h>
+#include <chrono>
+#include <time.h>
 
 #include "buff_size.h"
 
-#define DUMMY_SEND_FLAG 0x400 // equals to MSG_SYN
+#define LOCAL_IP ("4.4.0.1")
+#define OUT_PORT_LOCAL ("0")
+
+#define USEC_RESOLUTION (200) /* in usec */
+#define USER_PACKET_PER_BURST (672)
+#define TOTAL_PACKET_PER_BURST (800.006696)
+#define PACKETS_PER_BURST (800)
+
+#define CHUNK_SIZE (1474)
+#define NUM_LOOPS (100000000L)
+
+#define DUMMY_SEND_FLAG (0x400) /* equals to MSG_SYN */
 #ifndef SO_MAX_PACING_RATE
-#define SO_MAX_PACING_RATE 47
+#define SO_MAX_PACING_RATE (47)
 #endif
+
+#define NOW() std::chrono::system_clock::now()
+#define SUB_MILI(X, Y) std::chrono::duration_cast<std::chrono::milliseconds>(X - Y).count()
 
 void error(const char *msg) {
 	perror(msg);
@@ -71,84 +87,112 @@ Exit:
 	return ret;
 }
 
-int main(int argc, char *argv[])
+static int run(int sockfd, struct sockaddr *sa, struct sockaddr *sa_local, socklen_t salen, socklen_t salen_local)
 {
-	int sockfd, n, rc;
-	socklen_t salen, salen_local;
-	struct sockaddr *sa, *sa_local;
-	char *host, *port;
 	char *buffer;
+	uint64_t total_bytes = 0L;
+	int ret = -1;
+	float dummy_ratio = (TOTAL_PACKET_PER_BURST - USER_PACKET_PER_BURST) / TOTAL_PACKET_PER_BURST;
+	int dummies_sent = 0;
+	int next_dummies = 0;
+	std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+	std::chrono::milliseconds resolution(USEC_RESOLUTION);
 
 	if (!(buffer = (char*)malloc(CHUNK_SIZE))) {
 		printf("Could not allocate data buffer\n");
 		return -1;
 	}
 
-	if (argc < 3 || argc > 4) {
-		printf("usage: %s <hostname/IPaddress> <portnumber> [rate] \n",
-				argv[0]);
-		exit(-1);
-	}
-
-	host = argv[1];
-	port = argv[2];
-
-	my_getaddrinfo(host, port, &sockfd, &sa, &salen);
-        my_getaddrinfo("5.5.0.1", "0", &sockfd, &sa_local, &salen_local);
-
-
-	if (argc == 4) {
-		uint64_t rate = atol(argv[3]);
-
-		if (setsockopt(sockfd, SOL_SOCKET, SO_MAX_PACING_RATE, &rate,
-				sizeof(rate))) {
-			printf("setsockopt() failed \n");
-		}
-		printf("settings rate to %ul\n", rate);
-	}
-
-	uint64_t total_bytes = 0L;
-	float dummy_ratio = (800.006696 - 672) / 800.006696;
-	int dummies_sent = 0;
-	int next_dummies = 0;
 	for (int i = 0; i < NUM_LOOPS; ++i) {
-		for (int j = 0; j < 672; ++j) {
+		start = NOW();
+		for (int j = 0; j < USER_PACKET_PER_BURST; ++j) {
 			if (sendto(sockfd, buffer, CHUNK_SIZE, 0, sa, salen) < 0) {
-				/* buffers aren't available locally at the moment,
-				 * try again.
-				 */
+				/* buffers aren't available locally at the moment */
 				if (errno == ENOBUFS) {
 					printf("sendto error ENOBYFS");
 					fflush(stdout);
 					continue;
 				}
 				perror("error sending datagram");
-				exit(1);
+				ret = -1;
+				goto Exit;
 			}
+
+			/* inject dummy packets 10 times per burst */
 			if (!(j % (PACKETS_PER_BURST/10))) {
-				next_dummies = floor((i * 800.006696 + j) * dummy_ratio - dummies_sent);
+				next_dummies = floor((i * TOTAL_PACKET_PER_BURST + j) * dummy_ratio - dummies_sent);
 				for (int k = 0; k <= next_dummies; ++k) {
 					if (sendto(sockfd, buffer, CHUNK_SIZE, 0, sa_local, salen_local) < 0) {
-                                		/* buffers aren't available locally at the moment*/
-                                		if (errno == ENOBUFS){
-                                       			printf("sendto error ENOBYFS");
+						/* buffers aren't available locally at the moment*/
+						if (errno == ENOBUFS){
+							printf("sendto error ENOBYFS");
 							fflush(stdout);
-                                        		continue;
+							continue;
 						}
-                                		perror("error sending dummy datagram");
-                                		exit(1);
+						perror("error sending dummy datagram");
+						ret = -1;
+						goto Exit;
 					}
 				}
 				dummies_sent += next_dummies;
 			}
 			total_bytes += CHUNK_SIZE;
 		}
-		usleep(200000);
+		end = NOW();
+		usleep(USEC_RESOLUTION - SUB_MILI(end, start) - 10 /* slightly reduce sleep time */);
+
+		while (start + resolution > NOW()); /* busy wait for the correct time */
 	}
 	printf("\n %lu bytes sent \n", total_bytes);
+	ret = 0;
+
+Exit:
+	free(buffer);
+	return ret;
+}
+
+int main(int argc, char *argv[]) {
+	int sockfd = -1, rc;
+	socklen_t salen, salen_local;
+	struct sockaddr *sa = NULL, *sa_local = NULL;
+	char *host, *port;
+	int ret = -1;
+
+	if (argc < 3 || argc > 4) {
+		printf("usage: %s <hostname/IPaddress> <portnumber> [rate] \n", argv[0]);
+		exit(-1);
+	}
+
+	host = argv[1];
+	port = argv[2];
+
+	if (my_getaddrinfo(host, port, &sockfd, &sa, &salen))
+		goto Exit;
+	if (my_getaddrinfo(LOCAL_IP, OUT_PORT_LOCAL, NULL, &sa_local, &salen_local))
+		goto Exit;
+
+	if (argc == 4) {
+		uint64_t rate = atol(argv[3]);
+
+		printf("settings rate to: %lu\n", rate);
+		if (setsockopt(sockfd, SOL_SOCKET, SO_MAX_PACING_RATE, &rate, sizeof(rate))) {
+			printf("setsockopt() failed \n");
+			goto Exit;
+		}
+	}
+
+	if (run(sockfd, sa, sa_local, salen, salen_local) < 0) {
+		printf("program exit on error.\n");
+		goto Exit;
+	}
+
+	ret = 0;
+
+Exit:
 	free(sa);
 	free(sa_local);
-	free(buffer);
-	close(sockfd);
-	return 0;
+	if (-1 < sockfd)
+		close(sockfd);
+	return ret;
 }
+
