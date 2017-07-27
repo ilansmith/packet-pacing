68770ef43228734ca39cffe6448c380a4c5ae684
Or Shalev <orsh@mellanox.com>
2017-07-27 12:47:29 +0000

    fixed sleep accuracy
    Signed-off-by: Or Shalev <orsh@mellanox.com>

--- udp_client.cpp
+++ udp_client.cpp
@@ -12,33 +12,33 @@
 #include <math.h>
 #include <chrono>
 #include <time.h>
-#include <sys/time.h>
 
 
 #define LOCAL_IP ("4.4.0.1")
 #define OUT_PORT_LOCAL ("20654")
 
-#define USEC_RESOLUTION (200) /* in usec */
-#define USER_PACKET_PER_BURST (641)
+#define USEC_RESOLUTION (200000) /* in nanosec */
 
-#define MTU_PACKETS true
-/* Headers: 42 bytes
- */
+#define MTU_PACKETS 1
+
+/* Headers: 42 bytes */
 #if MTU_PACKETS
-#define TOTAL_PACKET_PER_BURST (762.945838838)
-#define CHUNK_SIZE (1472)
+#define USER_PACKET_PER_BURST (641)
+#define TOTAL_PACKET_PER_BURST (762.945838838)/*(800.006695905)*/
+#define CHUNK_SIZE (1472) /*1514-42*/
 #else
-#define TOTAL_PACKET_PER_BURST (683.322324967)
+#define USER_PACKET_PER_BURST (716)
+#define TOTAL_PACKET_PER_BURST (762.942477876)/*(800.003171681)*/
 #define CHUNK_SIZE (1314) /*1356-42*/
 #endif
 
 #define NUM_LOOPS (100000000L)
 
 #define DUMMY_SEND_FLAG (0x400) /* equals to MSG_SYN */
+#define DUMMY_SPOOF_MAC (0x44000000)
 
-
-#define NOW() std::chrono::system_clock::now()
-#define SUB_MILI(X, Y) std::chrono::duration_cast<std::chrono::milliseconds>(X - Y).count()
+#define NOW() std::chrono::high_resolution_clock::now()
+#define SUB_MICRO(X, Y) std::chrono::duration_cast<std::chrono::microseconds>(X - Y).count()
 
 void error(const char *msg) {
 	perror(msg);
@@ -93,14 +93,6 @@ Exit:
 	return ret;
 }
 
-static inline unsigned long long int time_get_usec()
-{
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	return (((unsigned long long int) tv.tv_sec * 1000000LL)
-			+ (unsigned long long int) tv.tv_usec);
-}
-
 static int run(int sockfd, struct sockaddr *sa, struct sockaddr *sa_local, socklen_t salen, socklen_t salen_local)
 {
 	char *buffer;
@@ -110,8 +102,9 @@ static int run(int sockfd, struct sockaddr *sa, struct sockaddr *sa_local, sockl
 	float counter_next_dummy = 0;
 	int dummies_sent = 0;
 	int next_dummies = 0;
-	std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-	std::chrono::milliseconds resolution(USEC_RESOLUTION);
+	std::chrono::time_point<std::chrono::high_resolution_clock> start = NOW();
+	std::chrono::time_point<std::chrono::high_resolution_clock> end;
+	std::chrono::microseconds resolution(USEC_RESOLUTION);
 
 	if (!(buffer = (char*)malloc(CHUNK_SIZE))) {
 		printf("Could not allocate data buffer\n");
@@ -119,9 +112,6 @@ static int run(int sockfd, struct sockaddr *sa, struct sockaddr *sa_local, sockl
 	}
 
 	for (int i = 0; i < NUM_LOOPS; ++i) {
-		start += NOW();
-		/*if (!(total_bytes % (5 * USER_PACKET_PER_BURST)))
-			std::cout << "now: " << time_get_usec() << std::endl;*/
 		for (int j = 0; j < USER_PACKET_PER_BURST; ++j) {
 			if (sendto(sockfd, buffer, CHUNK_SIZE, 0, sa, salen) < 0) {
 				/* buffers aren't available locally at the moment */
@@ -136,9 +126,9 @@ static int run(int sockfd, struct sockaddr *sa, struct sockaddr *sa_local, sockl
 			}
 
 			counter_next_dummy += dummy_ratio;
-			if (dummy_ratio > 10.0) {
-				counter_next_dummy -= 10.0;
-				if (sendto(sockfd, buffer, CHUNK_SIZE, DUMMY_SEND_FLAG, sa, salen) < 0) {
+			if (dummy_ratio > 1.0) {
+				counter_next_dummy -= 1.0;
+				if (sendto(sockfd, buffer, CHUNK_SIZE, DUMMY_SEND_FLAG, sa_local, salen_local) < 0) {
 					// buffers aren't available locally at the moment
 					if (errno == ENOBUFS){
 						printf("sendto error ENOBYFS");
@@ -153,9 +143,10 @@ static int run(int sockfd, struct sockaddr *sa, struct sockaddr *sa_local, sockl
 			total_bytes += CHUNK_SIZE;
 		}
 		end = NOW();
-		usleep(USEC_RESOLUTION - SUB_MILI(end, start) - 10 /* slightly reduce sleep time */);
+		usleep(USEC_RESOLUTION - SUB_MICRO(end, start) - 10/* slightly reduce sleep time */);
 
 		while (start + resolution > NOW()); /* busy wait for the correct time */
+		start = start + resolution;
 	}
 	printf("\n %lu bytes sent \n", total_bytes);
 	ret = 0;
